56c8189a94470e232d5f0bed6719777785407528
Matt Robinson <brimstone@the.narro.ws>
2017-02-25 16:01:15 -0500

    Fix tests

--- test_endpoints.py
+++ test_endpoints.py
@@ -2,14 +2,17 @@ from app import app
 
 client = app.test_client()
 
+
 def test_base_url():
-  rsp = client.get('/')
-  assert rsp.status == '200 OK'
+    rsp = client.get('/')
+    assert rsp.status == '200 OK'
+
 
 def test_buses():
-    rsp_bus = client.get('/v1/bus')
+    rsp_bus = client.get('/v1/buses')
     assert rsp_bus.status == '200 OK'
 
+
 def test_routes():
-    rsp_routes = client.get('/v1/route')
+    rsp_routes = client.get('/v1/routes')
     assert rsp_routes.status == '200 OK'
