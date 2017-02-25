e64293b48f51fc5c23b3622d268f277db67f2c2f
Matt Robinson <brimstone@the.narro.ws>
2017-02-24 23:02:03 -0500

    Made it do a thing!

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# if __name__ == '__main__':
+app.run(host='0.0.0.0', port=os.getenv('PORT'))
