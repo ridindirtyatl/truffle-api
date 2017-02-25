445dce71782f95750a1cf8dc40448e2a0b31dd1c
Gargi <gsrajadh@ncsu.edu>
2017-02-25 03:43:39 -0500

    Add tests for endpoints

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def raml():
     return send_file('API.raml')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT')))
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT',5000)))
